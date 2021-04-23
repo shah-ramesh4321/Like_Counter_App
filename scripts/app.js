50868b58b2ea4ffb977f55062abbc90a8d56b43a
Ramesh kumar shah <37567180+shah-ramesh4321@users.noreply.github.com>
2021-04-23 15:00:08 +0530

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+
+window.addEventListener('DOMContentLoaded', bindEvents);
+
+function bindEvents(){
+    document.querySelector('#like').addEventListener('click', plusCounter);
+    // document.getElementById('count').innerHTML = '0';
+}
+var counter = 0;
+
+function plusCounter(){
+    counter++; 
+    document.querySelector('#count').innerHTML = `<strong> ${counter} </strong>`;
+    console.log(counter);
+}
